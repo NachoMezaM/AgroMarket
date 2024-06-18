708395a8bc7d70cbc5bcbfba536069251477ab8f
NachoMezaM <nachom1605@gmail.com>
2024-06-18 00:27:06 -0400

    AgroMarket

---
+++ app/src/main/java/com/example/agromarket/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.agromarket
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnlogin = findViewById<Button>(R.id.login)
+        btnlogin.setOnClickListener{
+            val intent = Intent(
+                this@MainActivity,
+                isesion::class.java
+            )
+            startActivity(intent)
+        }
+    }
+}
